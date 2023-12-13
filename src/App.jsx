8d6fdfde4ae260a4c43e7e9cb88bf422a5fabaff
AyeshaSeemab93 <ayeshaseemab2017@gmail.com>
2023-12-13 13:19:17 +0200

    modify code

--- src/App.jsx
+++ src/App.jsx
@@ -20,15 +20,7 @@ function App(){
 
     useEffect(()=>{
       console.log("using effect")
-      const mockResponse = [
-        {
-          name: 'Ayesha',
-          number: '333-456-123',
-          id: 1, // You can assign a unique id
-        },
-      ];
-    
-    setPersons(mockResponse);
+
     numberService
       .getAll()
       .then(response=>{
